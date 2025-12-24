97ecc4b22c0250a6d35136a86c984fbebc330893
mseptiawan <mseptiawan017@gmail.com>
2025-12-25 02:08:10 +0700

    sae

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import attendanceRoutes from "./modules/attendance/attendance.routes";
-
-const router = Router();
-router.use("/attendance", attendanceRoutes);
-
-export default router;
